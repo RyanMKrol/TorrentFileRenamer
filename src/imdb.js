b0b551c00648d64b9b219f63e2c7630df94950fd
RyanMKrol <ryankrol.m@gmail.com>
2022-09-24 00:28:08 +0100

    Add utility file for imdb functions

---
+++ src/imdb.js
@@ -0,0 +1,44 @@
+import curl from 'curl';
+import cheerio from 'cheerio';
+
+/**
+ * Utility to get an IMDb link which points to the episode list for
+ * a given season of a show
+ *
+ * @param {number} seriesIndex The series number to get a link for
+ * @param {string} seriesId The ID of the series to get a link for
+ *
+ * @returns {string} The URL to find episode information for
+ */
+function getSeriesLink(seriesIndex, seriesId) {
+  return `https://www.imdb.com/title/${seriesId}/episodes?season=${seriesIndex}`;
+}
+
+/**
+ * Method to get episode names from an IMDb page
+ *
+ * @param {string} seriesLink The link to find episode names from
+ * @returns {Array<string>} An array of episode names
+ */
+async function getEpisodeNames(seriesLink) {
+  return new Promise((resolve, reject) => {
+    curl.get(seriesLink, (err, response, body) => {
+      try {
+        const $ = cheerio.load(body);
+        const episodeNames = $('.eplist .info strong')
+          .map((_, element) => $(element).text())
+          .get();
+
+        resolve(episodeNames);
+      } catch (error) {
+        reject(
+          new Error(
+            `Could not grab number of pages needed from page: ${seriesLink}, error: ${error}`,
+          ),
+        );
+      }
+    });
+  });
+}
+
+export { getSeriesLink, getEpisodeNames };
